d7f56dc3269f5b0c92472a8b11bfea4b60d8ea5c
Kheoh Yee Wei <kwei88@gmail.com>
2019-10-04 13:54:14 +0800

    stuff

--- rollup.config.js
+++ rollup.config.js
@@ -1,65 +1,62 @@
-import pkg from './package.json'
-import { terser } from 'rollup-plugin-terser'
-import resolve from 'rollup-plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
-import postcss from 'rollup-plugin-postcss'
+import pkg from "./package.json";
+import { terser } from "rollup-plugin-terser";
+import resolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import postcss from "rollup-plugin-postcss";
+import common from "rollup-plugin-commonjs";
 
 const plugins = [
   babel({
-    exclude: 'node_modules/**', // only transpile our source code
+    exclude: "node_modules/**", // only transpile our source code
     comments: false
   }),
   resolve(),
+  common(),
   postcss({
     extract: true
   }),
   terser()
-]
+];
 
 export default [
   {
-    input: 'src/Datepicker.js',
-    external: ['preact'],
+    input: "src/Datepicker.js",
+    external: ["preact"],
     output: [
       {
         file: pkg.main,
-        format: 'cjs',
-        globals: {
-          preact: 'preact'
-        },
+        format: "cjs",
         sourcemap: true
-      },
-      {
-        file: pkg.browser,
-        format: 'umd',
-        name: 'DatePickerDate',
-        globals: {
-          preact: 'preact'
-        }
       },
       {
         file: pkg.module,
-        format: 'esm',
-        globals: {
-          preact: 'preact'
-        },
+        format: "esm",
         sourcemap: true
+      },
+      {
+        file: pkg.umd,
+        format: "umd",
+        name: "DatePickerDate",
+        sourcemap: true,
+        globals: {
+          preact: "preact"
+        }
       }
     ],
     plugins
   },
   {
-    input: 'src/app.js',
+    input: "src/app.js",
     output: [
       {
-        file: 'src/app.min.js',
-        format: 'umd',
-        name: 'DatePickerDate',
+        file: "src/app.min.js",
+        format: "umd",
+        name: "DatePickerDate",
         globals: {
-          preact: 'preact'
+          preact: "preact"
         }
       }
     ],
     plugins
   }
-]
+];
--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import { h, Component, render } from 'preact'
 import { Datepicker } from './Datepicker'
 import './Calendar.css'
+import { WEEK_DAYS } from './calendar-helpers'
+
+function formatDate (dateStr) {
+  if (!dateStr) return
+  const date = new Date(dateStr)
+  return `${WEEK_DAYS[date.getDay()]} ${date.getDate()}`
+}
 
 class DatepickerDate extends Component {
   handleChange = (name, value) => {
@@ -10,10 +17,11 @@ class DatepickerDate extends Component {
   render () {
     return (
       <Datepicker
-        name='date'
-        label='Date'
-        value='2019-02-11'
+        name="date"
+        label="Date"
+        value="2019-02-11"
         onDateChanged={this.handleChange}
+        formatter={formatDate}
       />
     )
   }
